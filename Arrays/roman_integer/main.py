142dd08a37553ff9e75952afac8ed5badd5edf17
jafarHussein <sanjaygreg19@gmail.com>
2026-05-15 13:03:02 +0300

    we solved the longest common prefix, working on valid parenthesis

--- Arrays/roman_integer/main.py
+++ Arrays/roman_integer/main.py
@@ -1,33 +1,50 @@
 class Solution:
-    def romanToInt(self, s):
-        roman_values={"M":1000,
+    def romanToInt(self,s):
+        individual_letter_list=list(s)
+        roman_values=roman_values={"M":1000,
               "D":500,
               "C":100,
               "L":50,
               "X":10,
               "V":5,
               "I":1}
-        individual_letter_list=list(s)
-        empty_list=[]
+        skip_next=False
         sum=0
+        empty_list=[]
+        
         for i in range(0, len(individual_letter_list)):
+            if skip_next:
+                skip_next=False
+                continue
+            found_subtraction=False
             for j in range(i+1, len(individual_letter_list)):
                 current_value=roman_values.get(individual_letter_list[i])
                 next_value=roman_values.get(individual_letter_list[j])
-                if current_value > next_value:
-                    empty_list.append(current_value)
-                else:
+               
+                if next_value > current_value:
                     empty_list.append(next_value-current_value)
+                    skip_next=True
+                    found_subtraction=True
                 break
-            
+                    
+            if not found_subtraction:
+                    empty_list.append(roman_values.get(individual_letter_list[i]))
+                    
         for value in empty_list:
             sum+=value
             
         return sum
-        
-
+           
+    
 sol=Solution()
 print(sol.romanToInt("MCMXCIV"))
+            
+            
+        
+                    
+                
+                   
+                    
             
         
         
