cad46336387cedfdc6396f33b0907af4ae2bfee1
jafarHussein <sanjaygreg19@gmail.com>
2026-07-06 10:30:19 +0300

    valid palindrome problem

---
+++ Strings/valid_palindrome/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def ispalindrome(string):
+        if not isinstance(string, (str, float, int)):
+            return False
+        string=string.lower()
+        new_string="".join(char for char in string if char.isalnum())
+        is_pal=new_string == new_string[::-1]
+        return is_pal
+
+s="A man, a plan, a canal: Panama"
+    
+print(Solution.ispalindrome(s))
